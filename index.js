31602e5a5674444609747abf94794f5050d60268
shrinivas.patil <shrinivas.patil@harbingergroup.com>
2023-12-10 16:22:34 +0530

    Add .gitignore to ignore node_modules

---
+++ index.js
@@ -0,0 +1,39 @@
+const express = require("express");
+const cors = require("cors");
+const mongoose = require("mongoose");
+const authRoutes = require("./routes/authRoutes");
+const cookieParser = require("cookie-parser");
+
+const app = express();
+
+app.listen(process.env.PORT || 4000, (err) => {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log("Server Started Successfully.");
+  }
+});
+
+mongoose
+  .connect("mongodb+srv://dhanashreepatil840:rutApOP70ytSqAaC@test-jwt-db.lwhmmpw.mongodb.net/?retryWrites=true&w=majority", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("DB Connetion Successfully");
+  })
+  .catch((err) => {
+    console.log(err.message);
+  });
+
+app.use(
+  cors({
+    origin: ["http://localhost:3000"],
+    methods: ["GET", "POST"],
+    credentials: true,
+  })
+);
+app.use(cookieParser());
+
+app.use(express.json());
+app.use("/", authRoutes);
